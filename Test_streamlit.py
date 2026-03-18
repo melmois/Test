9a558cfa1bf53a06763ac9993f1e3d2198d9fec2
melmois <melaniemois0802@gmail.com>
2026-03-18 19:39:19 +0100

    Update Test_streamlit.py

--- Test_streamlit.py
+++ Test_streamlit.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 # Configuration de la page
 st.set_page_config(page_title="Tableau de Bord V2", page_icon="🚀")
